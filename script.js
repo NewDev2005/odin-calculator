e5f06488be5d7b54149bbd7b626d10cfcdd1844c
krypto <clayjeansen3@gmail.com>
2024-03-10 01:02:17 +0530

    operate has been added

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ function divide(num1, num2){
     return console.log(num1 / num2);
 };
 
+let operator;
+let operand1;
+let operand2;
+
+function operate(number1, oprtr, number2){
+if(oprtr === '+'){
+ return add(number1,number2);
+}
+else if(oprtr === '-'){
+    return substract(number1,number2);
+}
+else if(oprtr === '*'){
+    return product(number1,number2);
+}
+else if(oprtr === '/'){
+    return divide(number1,number2);
+}
+}
+
 
