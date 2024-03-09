e7900f1f2403cad00760dfd5efb97180ba47c3d0
krypto <clayjeansen3@gmail.com>
2024-03-09 23:28:05 +0530

    All mathematical operators have been added

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(num1,num2){
+return console.log(num1 + num2);
+};
+
+function substract(num1,num2){
+    return console.log(num1 - num2);
+};
+
+function product(num1, num2){
+    return console.log(num1 * num2);
+};
+
+function divide(num1, num2){
+    return console.log(num1 / num2);
+};
+
+
