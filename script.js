6f26d196b9c5ba1929cda45cbb77c4e2f40ae297
krypto <clayjeansen3@gmail.com>
2024-03-10 19:10:58 +0530

    calculator buttons have been formed

--- script.js
+++ script.js
@@ -32,5 +32,65 @@ else if(oprtr === '/'){
     return divide(number1,number2);
 }
 }
+function displayText(){
+    const displayOutput = document.createElement('div');
+    const para = document.createElement('p');
+    displayOutput.classList.add('display-para');
+    const container = document.querySelector('#container');
+    displayOutput.appendChild(para);
+    container.appendChild(displayOutput);
+}
+displayText();
 
+const btnContainer = document.createElement('div');
+btnContainer.classList.add('btn-container');
+document.getElementById('container').appendChild(btnContainer);
 
+function digitBtn(){
+    const div = document.createElement('div');
+    const btn = document.createElement('button');
+    btn.classList.add('digit');
+    div.appendChild(btn);
+    document.querySelector('.btn-container').appendChild(div);
+}
+function calcBtn(){
+    for(let i=0; i<=15; i++){
+        digitBtn();
+    };
+}
+calcBtn();
+
+function insertTextInBtn(){
+    const container = document.getElementById('container');
+    const btnContainer = container.querySelector('.btn-container');
+    const btnChild = btnContainer.getElementsByClassName('digit');
+    
+    for(let i=0; i<btnChild.length; i++){
+        for(let j=0; j<=15; j++){
+            if(j === 10){
+                btnChild[j].textContent = '➕';
+            }
+            else if(j === 11){
+                btnChild[j].textContent = '➖';
+            }
+            else if(j === 12){
+                btnChild[j].textContent = '✖️';
+            }
+            else if(j === 13){
+                btnChild[j].textContent = '➗';
+            }
+            else if(j === 14){
+                btnChild[j].textContent = '🟰';
+            }
+            else if(j === 15){
+                btnChild[j].textContent = 'Clear';
+            }
+           
+            else{
+                btnChild[j].textContent = j;
+            }
+           
+        }
+    }
+}
+insertTextInBtn();
