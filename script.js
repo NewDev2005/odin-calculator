abf914f2dcc52f0ad6b03f082e8ad688380a038b
krypto <clayjeansen3@gmail.com>
2024-03-11 22:19:21 +0530

    Reconfigure the calc  button layout and added a variable to store the screen textContent

--- script.js
+++ script.js
@@ -43,6 +43,8 @@ function displayText(){
 }
 displayText();
 
+
+
 const btnContainer = document.createElement('div');
 btnContainer.classList.add('btn-container');
 document.getElementById('container').appendChild(btnContainer);
@@ -66,36 +68,109 @@ function insertTextInBtn(){
     const container = document.getElementById('container');
     const btnContainer = container.querySelector('.btn-container');
     const btnChild = btnContainer.getElementsByClassName('digit');
-    
-    for(let i=0; i<btnChild.length; i++){
-        for(let j=0; j<=16; j++){
-            if(j === 10){
-                btnChild[j].textContent = '+';
-            }
-            else if(j === 11){
-                btnChild[j].textContent = '-';
-            }
-            else if(j === 12){
-                btnChild[j].textContent = '*';
-            }
-            else if(j === 13){
-                btnChild[j].textContent = '/';
-            }
-            else if(j === 14){
-                btnChild[j].textContent = '=';
-            }
-            else if(j === 15){
-                btnChild[j].textContent = '.';
-            }
-            else if(j === 16){
-                btnChild[j].textContent = 'C'
-            }
-           
-            else{
-                btnChild[j].textContent = j;
-            }
-           
-        }
-    }
+     
+        btnChild[0].textContent = 7;
+        btnChild[1].textContent = 8;
+        btnChild[2].textContent = 9;
+        btnChild[3].textContent = 4;
+        btnChild[4].textContent = 5;
+        btnChild[5].textContent = 6;
+        btnChild[6].textContent = 1;
+        btnChild[7].textContent = 2;
+        btnChild[8].textContent = 3;
+        btnChild[9].textContent = 0;
+        btnChild[10].textContent = '+'
+        btnChild[11].textContent = '-';
+        btnChild[12].textContent = '=';
+        btnChild[13].textContent = '/';
+        btnChild[14].textContent = '*';
+        btnChild[15].textContent = '.';
+        btnChild[16].textContent = 'AC';
 }
 insertTextInBtn();
+
+let saveDisplayValue;
+
+function activateBtn(){
+    const container = document.getElementById('container');
+    const btnContainer = container.querySelector('.btn-container');
+    const btnChild = btnContainer.getElementsByClassName('digit');
+
+    btnChild[0].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 7;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[1].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 8;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[2].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 9;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[3].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 4;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[4].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 5;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[5].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 6;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[6].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 1;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[7].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 2;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[8].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 3;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[9].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = 0;
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[10].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = '+';
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[11].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = '-';
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[13].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = '/';
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[14].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = '*';
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+    btnChild[15].addEventListener("click",() => {
+        document.querySelector('.paragraph').textContent = '.';
+        saveDisplayValue = document.querySelector('.paragraph').textContent;
+    });
+
+}
+
+activateBtn();
